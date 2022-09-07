ee4b46073563cc82be612dea8db5cdd519d5c1e5
gkdud583 <hyng0115@gmail.com>
2022-09-07 16:11:03 +0900

    :recycle: CommentSaveRequestDto 기본 생성자 -> @JsonCreator 변경

--- src/main/java/com/lets/web/dto/comment/CommentSaveRequestDto.java
+++ src/main/java/com/lets/web/dto/comment/CommentSaveRequestDto.java
@@ -1,17 +1,15 @@
 package com.lets.web.dto.comment;
 
-import com.lets.domain.comment.Comment;
-import com.lets.domain.user.User;
-import com.lets.domain.post.Post;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class CommentSaveRequestDto {
-    private String content;
+  private String content;
 
-    public CommentSaveRequestDto(String content) {
-        this.content = content;
-    }
+  @JsonCreator
+  public CommentSaveRequestDto(String content) {
+    this.content = content;
+  }
 }
