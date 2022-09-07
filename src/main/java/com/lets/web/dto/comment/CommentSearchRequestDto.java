35aae421ee53ccda74bcbdf3118299b13b56dedd
gkdud583 <hyng0115@gmail.com>
2022-09-07 16:13:15 +0900

    :recycle: 코드 포맷팅

--- src/main/java/com/lets/web/dto/comment/CommentSearchRequestDto.java
+++ src/main/java/com/lets/web/dto/comment/CommentSearchRequestDto.java
@@ -1,13 +1,16 @@
 package com.lets.web.dto.comment;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.lets.domain.post.Post;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-@AllArgsConstructor
 public class CommentSearchRequestDto {
-    private Post post;
+  private Post post;
+
+  @JsonCreator
+  public CommentSearchRequestDto(Post post) {
+    this.post = post;
+  }
 }
