0594291ddbcea9fd05ae4ff594d95393dfd808c9
gkdud583 <hyng0115@gmail.com>
2022-09-07 16:09:37 +0900

    :recycle: CommentRepository 수정 & 테스트 수정

--- src/main/java/com/lets/domain/comment/CommentRepository.java
+++ src/main/java/com/lets/domain/comment/CommentRepository.java
@@ -1,13 +1,13 @@
 package com.lets.domain.comment;
 
-import com.lets.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.lets.domain.post.Post;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentCustomRepository {
-    Long countByPost(Post post);
+  Long countByPost(Post post);
 
-    void deleteAllByPost(Post post);
+  void deleteAllByPost(Post post);
 }
--- src/test/java/com/lets/domain/comment/CommentRepositoryTest.java
+++ src/test/java/com/lets/domain/comment/CommentRepositoryTest.java
@@ -1,12 +1,9 @@
 package com.lets.domain.comment;
 
-import com.lets.config.QueryDslConfig;
-import com.lets.domain.post.Post;
-import com.lets.domain.post.PostRepository;
-import com.lets.domain.user.User;
-import com.lets.domain.user.UserRepository;
-import com.lets.security.oauth2.AuthProvider;
-import com.lets.web.dto.comment.CommentSearchRequestDto;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,76 +11,79 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.lets.config.QueryDslConfig;
+import com.lets.domain.post.Post;
+import com.lets.domain.post.PostRepository;
+import com.lets.domain.user.User;
+import com.lets.domain.user.UserRepository;
+import com.lets.security.oauth2.AuthProvider;
+import com.lets.web.dto.comment.CommentSearchRequestDto;
 
 @DataJpaTest
 @Import(QueryDslConfig.class)
 public class CommentRepositoryTest {
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Autowired
-    private CommentRepository commentRepository;
+  @Autowired
+  private CommentRepository commentRepository;
 
-    @Autowired
-    private PostRepository postRepository;
+  @Autowired
+  private PostRepository postRepository;
 
-    private User user;
-    private Post post;
-    private Comment comment;
+  private User user;
+  private Post post;
+  private Comment comment;
 
-    @BeforeEach
-    void setup(){
-        user = User.createUser("user1", "123", AuthProvider.google, "default");
-        userRepository.save(user);
+  @BeforeEach
+  void setup() {
+    user = User.createUser("user1", "123", AuthProvider.google, "default");
+    userRepository.save(user);
 
-        post = Post.createPost(user, "title1", "content1");
-        postRepository.save(post);
+    post = Post.createPost(user, "title1", "content1");
+    postRepository.save(post);
 
-        comment = Comment.createComment(user, post, "comment1");
-        commentRepository.save(comment);
-    }
+    comment = Comment.createComment(user, post, "comment1");
+    commentRepository.save(comment);
+  }
 
-    @DisplayName("글의 댓글 수를 반환합니다.")
-    @Test
-    public void findByName(){
-        //given
-        //when
-        Long result = commentRepository.countByPost(post);
+  @DisplayName("글의 댓글 수를 반환합니다.")
+  @Test
+  public void countByPost() {
+    //given
 
-        //then
-        assertThat(result).isEqualTo(1);
-    }
+    //when
+    Long count = commentRepository.countByPost(post);
 
-    @DisplayName("특정 글의 모든 댓글을 삭제합니다.")
-    @Test
-    public void deleteAllByPost(){
-        //given
+    //then
+    assertThat(count).isEqualTo(1);
+  }
 
-        //when
-        commentRepository.deleteAllByPost(post);
+  @DisplayName("특정 글의 모든 댓글을 삭제합니다.")
+  @Test
+  public void deleteAllByPost() {
+    //given
 
-        //then
-        Long result = commentRepository.countByPost(post);
-        assertThat(result).isEqualTo(0);
-    }
+    //when
+    commentRepository.deleteAllByPost(post);
 
-    @DisplayName("모든 댓글을 조회합니다.")
-    @Test
-    public void findComments(){
-        //given
-        Comment comment2 = Comment.createComment(user, post, "comment2");
-        commentRepository.save(comment2);
+    //then
+    Long result = commentRepository.countByPost(post);
+    assertThat(result).isEqualTo(0);
+  }
 
-        //when
-        List<Comment> comments = commentRepository.findComments(new CommentSearchRequestDto(post));
+  @DisplayName("모든 댓글을 조회합니다.")
+  @Test
+  public void findComments() {
+    //given
+    Comment comment2 = Comment.createComment(user, post, "comment2");
+    commentRepository.save(comment2);
 
-        //then
-        assertThat(comments.size()).isEqualTo(2);
-    }
+    //when
+    List<Comment> comments = commentRepository.findComments(new CommentSearchRequestDto(post));
 
+    //then
+    assertThat(comments.size()).isEqualTo(2);
+  }
 
 }
