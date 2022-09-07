0ee9bb18a7e7ef5f60ecb3a13a85ba6ea2347f76
gkdud583 <hyng0115@gmail.com>
2022-09-07 16:10:22 +0900

    :recycle: CommentResponseDto 기본 생성자 생성, 접근 지정자 변경

--- src/main/java/com/lets/web/dto/comment/CommentResponseDto.java
+++ src/main/java/com/lets/web/dto/comment/CommentResponseDto.java
@@ -1,32 +1,39 @@
 package com.lets.web.dto.comment;
 
+import java.time.LocalDateTime;
+
 import com.lets.domain.comment.Comment;
-import com.lets.domain.user.User;
-import com.lets.util.CloudinaryUtil;
+
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentResponseDto {
-    private String profile;
-    private Long id;
-    private String nickname;
-    private String content;
-    private LocalDateTime createdTime;
+  private String profile;
+  private Long id;
+  private String nickname;
+  private String content;
+  private LocalDateTime createdTime;
 
-    public CommentResponseDto(Comment comment, String profile){
-        this.profile = profile;
-        this.id = comment.getId();
-        this.nickname = comment.getUser().getNickname();
-        this.content = comment.getContent();
-        this.createdTime = comment.getCreatedDate();
-    }
+  private CommentResponseDto(
+      Comment comment,
+      String profile
+  ) {
+    this.profile = profile;
+    this.id = comment.getId();
+    this.nickname = comment
+        .getUser()
+        .getNickname();
+    this.content = comment.getContent();
+    this.createdTime = comment.getCreatedDate();
+  }
 
-    public static CommentResponseDto CommentToDto(Comment comment, String profile){
-        return new CommentResponseDto(comment, profile);
-    }
-
+  public static CommentResponseDto from(
+      Comment comment,
+      String profile
+  ) {
+    return new CommentResponseDto(comment, profile);
+  }
 }
