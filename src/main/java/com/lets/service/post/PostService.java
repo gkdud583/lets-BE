5a36ef39ff6fbaba5363bf444b2f5ce0c8ba26b7
gkdud583 <hyng0115@gmail.com>
2022-09-14 18:03:14 +0900

    :bug: 글 생성 시 likePost 생성하던 로직 삭제

--- src/main/java/com/lets/service/post/PostService.java
+++ src/main/java/com/lets/service/post/PostService.java
@@ -134,8 +134,6 @@ public class PostService {
     String profile = cloudinaryUtil.findFileURL(user.getPublicId());
     postTechStackRepository.saveAll(postTechStackList);
 
-    likePostRepository.save(LikePost.createLikePost(user, post));
-
     return PostResponseDto.PostToDto(post, tags, profile, 0L);
   }
 
